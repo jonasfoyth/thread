bd82c280f7991427d7b04bfa70d45750897ddbfb
Jonas Foyth <jonasfoyth@gmail.com>
2023-04-24 20:47:06 -0300

    changing threads

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,49 @@ use std::{thread, time};
 struct Car {
     id: Option<i32>,
     free_slots: Option<i32>,
+    running: bool,
+}
+
+#[derive(Clone, Debug)]
+
+struct Passenger {
+    can_board: bool,
+}
+
+impl Car {
+    fn initalize(id: Option<i32>, slots: Option<i32>) -> Self {
+        Car {
+            id: id,
+            free_slots: slots,
+            running: false
+        }
+    }
+
+    fn check_space(self) -> bool {
+        self.free_slots != None && self.free_slots != Some(0)
+    }
+
+    fn can_board(self) -> bool {
+        !self.running
+    }
+}
+
+impl Default for Car {
+    fn default() -> Self {
+        Car {
+            id: None,
+            free_slots: Some(CAR_SLOTS),
+            running: false
+        }
+    }
+}
+
+impl Default for Passenger {
+    fn default() -> Self {
+        Passenger {
+            can_board: false
+        }
+    }
 }
 
 static CAR_SLOTS: i32 = 4;
@@ -24,20 +67,8 @@ static mut CAR5: Car = Car::default();
 #[dynamic(drop)]
 static mut CAR6: Car = Car::default();
 #[dynamic(drop)]
-static mut PASSANGERS: i32 = i32::default();
+static mut PASSANGERS: Vec<Passenger> = Vec::new();
 
-impl Car {
-    fn initalize(id: Option<i32>, slots: Option<i32>) -> Self {
-        Car {
-            id: id,
-            free_slots: slots,
-        }
-    }
-
-    fn check_space(self) -> bool {
-        self.free_slots != None && self.free_slots != Some(0)
-    }
-}
 
 fn car_load(car_id: i8) {
     let mut free_passengers = PASSANGERS.write();
@@ -58,7 +89,7 @@ fn car_load(car_id: i8) {
     println!(":: free slots: {:?}", (*car_info).free_slots);
 
     if (*car_info).clone().check_space() {
-        if (*free_passengers) > 0 {
+        if (*free_passengers).len() > 0 {
             (*car_info).free_slots.clone_from(&None);
             (*free_passengers) -= passenger_board(CAR_SLOTS);
         };
@@ -72,7 +103,7 @@ fn car_load(car_id: i8) {
     }
 }
 
-fn car_unload(car_id: i32) {
+fn car_unload(car_id: i8) {
     let mut free_passengers = PASSANGERS.write();
 
     println! {"\n:: Free passengers: {}", (*free_passengers)};
@@ -90,20 +121,27 @@ fn car_unload(car_id: i32) {
     drop(car_info);
 }
 
-impl Default for Car {
-    fn default() -> Self {
-        Car {
-            id: None,
-            free_slots: Some(CAR_SLOTS),
+
+fn car_run(car_id: i8) {
+    println!(":: \nRunning car id {:?}", car_id);
+
+    let car_info = match car_id {
+        1 => CAR1.write(),
+        2 => CAR2.write(),
+        3 => CAR3.write(),
+        4 => CAR4.write(),
+        5 => CAR5.write(),
+        6 => CAR6.write(),
+    };
+
+
+    if let Some(free_slots) = (*car_info).free_slots {
+        if free_slots  == 0 {
+            (*car_info).running = true
         }
     }
-}
-
-fn car_run(id: Option<i32>) {
-    println!(":: \nRunning car id {:?}", id);
-    let mut randon = rand::thread_rng();
-    let timeout = randon.gen_range(1..5);
-    thread::sleep(time::Duration::from_secs(5));
+    
+    drop(car_info)
 
 }
 
@@ -115,12 +153,16 @@ fn passenger_unboard(passangers: i32) -> i32 {
     passangers
 }
 
+fn randon_number() -> u64 {
+    let mut randon = rand::thread_rng();
+    randon.gen_range(1..5)
+}
+
 fn main() {
     let mut rng = rand::thread_rng();
 
     let passangers = PASSANGERS.write();
     *passangers = rng.gen_range((6 * CAR_SLOTS)..100); // minimum passangers number fixed to (m*C)
-    drop(passangers);
     // Arc: share memory with all threads and mutex turn data lockable
 
     let mut handles: Vec<thread::JoinHandle<_>> = vec![];
@@ -131,11 +173,21 @@ fn main() {
 
         let handle = thread::spawn(move || loop {
             car_load(car_id);
-            car_run();
+            car_run(car_id);
+            thread::sleep(time::Duration::from_secs(randon_number()));
             car_unload(car_id);
         });
         handles.push(handle);
     }
+
+    for car_id in 1..(*passangers) {
+        thread::spawn(move || loop {
+            // println!("");
+
+        });
+    }
+
+    drop(passangers);
 
     loop {
         passangers
