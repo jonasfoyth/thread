d064fa782d62c2fd08bff0fdca2dd9c7ecf85ab0
Jonas Oscar Foyth <jonasoscar@weg.net>
2023-04-25 13:49:01 -0300

    fixing passanger queue

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use rand::Rng;
 use static_init::dynamic;
-use std::{thread, time};
+use std::{collections::VecDeque, thread, time};
 
 #[derive(Clone, Debug)]
 
@@ -8,12 +8,30 @@ struct Car {
     id: Option<i32>,
     free_slots: Option<i32>,
     running: bool,
+    can_board: bool,
 }
 
 #[derive(Clone, Debug)]
 
 struct Passenger {
-    can_board: bool,
+    id: Option<i32>,
+    can_load: bool,
+    boarded: bool,
+}
+
+impl Passenger {
+    fn initalize(number: i32) {
+        let mut free_passengers = PASSANGERS.write();
+        for id in 0..number {
+            (*free_passengers).push_back(Passenger {
+                id: Some(id),
+                can_load: false,
+                boarded: false,
+            });
+        }
+
+        drop(free_passengers);
+    }
 }
 
 impl Car {
@@ -21,7 +39,8 @@ impl Car {
         Car {
             id: id,
             free_slots: slots,
-            running: false
+            running: false,
+            can_board: false,
         }
     }
 
@@ -39,7 +58,8 @@ impl Default for Car {
         Car {
             id: None,
             free_slots: Some(CAR_SLOTS),
-            running: false
+            running: false,
+            can_board: false,
         }
     }
 }
@@ -47,7 +67,9 @@ impl Default for Car {
 impl Default for Passenger {
     fn default() -> Self {
         Passenger {
-            can_board: false
+            id: None,
+            can_load: false,
+            boarded: false,
         }
     }
 }
@@ -67,89 +89,99 @@ static mut CAR5: Car = Car::default();
 #[dynamic(drop)]
 static mut CAR6: Car = Car::default();
 #[dynamic(drop)]
-static mut PASSANGERS: Vec<Passenger> = Vec::new();
-
+static mut PASSANGERS: VecDeque<Passenger> = VecDeque::new();
 
 fn car_load(car_id: i8) {
     let mut free_passengers = PASSANGERS.write();
 
-    println! {"\n:: Free passengers: {}", (*free_passengers)};
+    println! {"\n:: Free passengers: {:?}", (*free_passengers)};
 
-    let car_info = match car_id {
+    let mut car_info = match car_id {
         1 => CAR1.write(),
         2 => CAR2.write(),
         3 => CAR3.write(),
         4 => CAR4.write(),
         5 => CAR5.write(),
         6 => CAR6.write(),
+        _ => return,
     };
 
     println! {"\n:: Car Status - before board"};
     println!(":: id: {:?}", (*car_info).id,);
     println!(":: free slots: {:?}", (*car_info).free_slots);
 
-    if (*car_info).clone().check_space() {
-        if (*free_passengers).len() > 0 {
-            (*car_info).free_slots.clone_from(&None);
-            (*free_passengers) -= passenger_board(CAR_SLOTS);
-        };
+    if (*free_passengers).len() > 0 {
+        if let Some(slots) = (*car_info).free_slots {
+            (*car_info).can_board = true;
+            // for _ in 1..slots {
+            //     let mut passenger : Passenger= (*free_passengers).swap_remove(0);
+            //     passenger.can_board = true;
+            //     (*free_passengers).push(passenger);
+            // }
+        } else {
+            (*car_info).can_board = false;
+        }
+    };
 
-        println! {"\n:: Car Status - after board"};
-        println!(":: id: {:?}", (*car_info).id,);
-        println!(":: free slots: {:?}", (*car_info).free_slots);
-        println! {":: after board Free passengers: {}", (*free_passengers)};
-        drop(car_info);
-        drop(free_passengers);
-    }
+    (*car_info).free_slots.clone_from(&None);
+
+    println! {"\n:: Car Status - after board"};
+    println!(":: id: {:?}", (*car_info).id,);
+    println!(":: free slots: {:?}", (*car_info).free_slots);
+    println! {":: after board Free passengers: {:?}", (*free_passengers)};
+    drop(car_info);
+    drop(free_passengers);
 }
 
 fn car_unload(car_id: i8) {
-    let mut free_passengers = PASSANGERS.write();
-
-    println! {"\n:: Free passengers: {}", (*free_passengers)};
-
-    let car_info = match car_id {
+    let mut car_info = match car_id {
         1 => CAR1.write(),
         2 => CAR2.write(),
         3 => CAR3.write(),
         4 => CAR4.write(),
         5 => CAR5.write(),
         6 => CAR6.write(),
+        _ => return,
     };
 
     (*car_info).free_slots.clone_from(&Some(CAR_SLOTS));
+    (*car_info).can_board.clone_from(&false);
     drop(car_info);
-}
 
+    // let mut free_passengers = PASSANGERS.write();
+    // (*free_passengers).iter().
+    // (*PASSANGERS).push();
+    // println! {"\n:: Free passengers: {:?}", (*free_passengers)};
+    // drop(free_passengers);
+}
 
 fn car_run(car_id: i8) {
     println!(":: \nRunning car id {:?}", car_id);
 
-    let car_info = match car_id {
+    let mut car_info = match car_id {
         1 => CAR1.write(),
         2 => CAR2.write(),
         3 => CAR3.write(),
         4 => CAR4.write(),
         5 => CAR5.write(),
         6 => CAR6.write(),
+        _ => return,
     };
 
-
     if let Some(free_slots) = (*car_info).free_slots {
-        if free_slots  == 0 {
+        if free_slots == 0 {
             (*car_info).running = true
         }
     }
-    
-    drop(car_info)
 
+    drop(car_info)
 }
 
-fn passenger_board(passangers: i32) -> i32 {
+fn passengers_board(passangers: i32) -> i32 {
     passangers
 }
 
-fn passenger_unboard(passangers: i32) -> i32 {
+fn passengers_unboard(passangers: i32) -> i32 {
     passangers
 }
 
@@ -160,10 +192,9 @@ fn randon_number() -> u64 {
 
 fn main() {
     let mut rng = rand::thread_rng();
-
-    let passangers = PASSANGERS.write();
-    *passangers = rng.gen_range((6 * CAR_SLOTS)..100); // minimum passangers number fixed to (m*C)
+    let passengers_number = rng.gen_range((6 * CAR_SLOTS)..100);
     // Arc: share memory with all threads and mutex turn data lockable
+    Passenger::initalize(passengers_number);
 
     let mut handles: Vec<thread::JoinHandle<_>> = vec![];
     // let mut cars = vec![];
@@ -180,16 +211,29 @@ fn main() {
         handles.push(handle);
     }
 
-    for car_id in 1..(*passangers) {
+    for person_id in 0..passengers_number {
         thread::spawn(move || loop {
-            // println!("");
+            let mut passangers = PASSANGERS.write();
+            println!("\n:: person id: {}", person_id);
 
+            if let Some(passenger) = (*passangers)
+                .iter()
+                .find(|&person| person.id == Some(person_id))
+            {
+                println!(":: passenger : {:?}", passenger);
+
+                if passenger.can_load {
+                    passengers_board(passenger.id.unwrap());
+                } else {
+                    passengers_unboard(passenger.id.unwrap());
+                };
+            }
+
+
+            // println!("");
+            drop(passangers);
         });
     }
 
-    drop(passangers);
-
-    loop {
-        passangers
-    }
+    loop {}
 }
